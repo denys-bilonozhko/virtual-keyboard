4898b4351e4feb2e3bf5a1ff1d15c50ae5db25d6
Denys Bilonozhko <deformix@gmail.com>
2023-04-23 17:25:30 +0300

    feat: add keys event listeners

--- src/components/keyboard/keyboard.js
+++ src/components/keyboard/keyboard.js
@@ -2,6 +2,8 @@ import './keyboard.scss';
 import keys from '../../keys';
 import keyboardRow from '../keyboardRow/keyboardRow';
 
+let language = 'eng';
+
 const keyboard = () => {
   const keyboardLayout = document.createElement('div');
   keyboardLayout.classList.add('keyboard');
@@ -33,6 +35,81 @@ const keyboard = () => {
       event.target.classList.remove('keyboard-button--active');
       event.target.classList.remove('keyboard-button--pressed');
     }
+  });
+
+  document.body.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.altKey) {
+      if (language === 'eng') {
+        document.querySelectorAll('.keyboard-button--symbol').forEach((key) => {
+          const currentKey = key;
+          currentKey.textContent = key.dataset.ru;
+        });
+        language = 'ru';
+      } else if (language === 'ru') {
+        document.querySelectorAll('.keyboard-button--symbol').forEach((key) => {
+          const currentKey = key;
+          currentKey.textContent = key.dataset.eng;
+        });
+        language = 'eng';
+      }
+    }
+  });
+
+  document.body.addEventListener('keydown', (event) => {
+    if (event.shiftKey) {
+      document.querySelectorAll('.keyboard-button--symbol').forEach((key) => {
+        const currentKey = key;
+        currentKey.textContent = key.dataset[`${language}upper`];
+      });
+    }
+  });
+
+  document.body.addEventListener('keyup', (event) => {
+    if (event.key === 'Shift') {
+      document.querySelectorAll('.keyboard-button--symbol').forEach((key) => {
+        const currentKey = key;
+        currentKey.textContent = key.dataset[language];
+      });
+    }
+  });
+
+  document.body.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('.keyboard-button--symbol').forEach((key) => {
+      if (e.keyCode === Number(key.dataset.keycode)) {
+        key.classList.add('keyboard-button--pressed');
+      }
+    });
+  });
+
+  document.body.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('.keyboard-button--symbol').forEach((key) => {
+      if (e.keyCode === Number(key.dataset.keycode)) {
+        key.classList.remove('keyboard-button--pressed');
+      }
+    });
+  });
+
+  document.body.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('.keyboard-button--service').forEach((key) => {
+      if (key.classList.contains(`keyboard-button--${e.code.toLowerCase()}`)) {
+        document
+          .querySelector(`.keyboard-button--${e.code.toLowerCase()}`)
+          .classList.add('keyboard-button--pressed');
+      }
+    });
+  });
+
+  document.body.addEventListener('keyup', (e) => {
+    document.querySelectorAll('.keyboard-button--service').forEach((key) => {
+      if (key.classList.contains(`keyboard-button--${e.code.toLowerCase()}`)) {
+        document
+          .querySelector(`.keyboard-button--${e.code.toLowerCase()}`)
+          .classList.remove('keyboard-button--pressed');
+      }
+    });
   });
 
   return keyboardLayout;
