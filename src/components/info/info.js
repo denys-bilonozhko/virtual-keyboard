12ae9379ebc3c1e5f8724edf2a6ccf82cf34c17a
Denys Bilonozhko <deformix@gmail.com>
2023-04-30 12:03:39 +0300

    feat: update info

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -8,7 +8,7 @@ const info = () => {
   div.className = 'info';
   p1.textContent = 'Windows OS';
   p2.textContent = 'To switch between keyboard layouts press Ctrl + Alt';
-  p3.textContent = 'Combinations Ctrl + A, Ctrl + X, Ctrl + C, Ctrl + V work as they should';
+  p3.textContent = 'The Ctrl + A, Ctrl + X, Ctrl + C, and Ctrl + V combinations work as they should';
   div.append(p1, p2, p3);
   return div;
 };
