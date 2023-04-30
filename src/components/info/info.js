315b60a3e66b3f39540dda06b2cb8ea234a4e864
Denys Bilonozhko <deformix@gmail.com>
2023-04-30 21:45:24 +0300

    feat: remove some info

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -4,12 +4,10 @@ const info = () => {
   const div = document.createElement('div');
   const p1 = document.createElement('p');
   const p2 = document.createElement('p');
-  const p3 = document.createElement('p');
   div.className = 'info';
   p1.textContent = 'Windows OS';
   p2.textContent = 'To switch between keyboard layouts press Ctrl + Alt';
-  p3.textContent = 'The Ctrl + A, Ctrl + X, Ctrl + C, and Ctrl + V combinations work as they should';
-  div.append(p1, p2, p3);
+  div.append(p1, p2);
   return div;
 };
 
